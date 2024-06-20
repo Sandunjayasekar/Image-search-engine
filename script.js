84068ad1ed7a0680b052cb024aa1b482ed3be92c
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2024-06-20 16:49:57 +0530

    all done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const searchForm = document.getElementById('search-form');
 const searchBox = document.getElementById('search-box');
 const searchResult = document.getElementById('search-result');
-const loadMore = document.getElementById('load-more');
+const loadMoreBtn = document.getElementById('load-more');
 
 // api key
 const apiKey = "VAcIY6TUR16osJDQLlVthTgoMA4vbaSSPw0_sqRL6bE";
@@ -9,9 +9,12 @@ const apiKey = "VAcIY6TUR16osJDQLlVthTgoMA4vbaSSPw0_sqRL6bE";
 let keyWord = "";
 let page = 1;
 
+if (page === 1) {
+    searchResult.innerHTML = "";
+}
 async function searchImages() {
     keyWord = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${apiKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${apiKey}&per_page=12`;
 
     const response = await fetch(url);
     const data = await response.json();
@@ -29,10 +32,17 @@ async function searchImages() {
 
         searchResult.appendChild(imageLink);
       });
+
+      loadMoreBtn.style.display = "block";
 }
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     page = 1;
+    searchImages();
+})
+
+loadMoreBtn.addEventListener('click', () => {
+    page++;
     searchImages();
 })

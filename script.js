e7a1b22764394339d9ee348f5cf9461e25811863
Sandunjayasekar <jmsrjayasekara97@gmail.com>
2024-06-20 16:24:42 +0530

    api added

---
+++ script.js
@@ -0,0 +1,15 @@
+const searchForm = document.getElementById('search-form');
+const searchBox = document.getElementById('search-box');
+const searchResult = document.querySelector('.search-result');
+const loadMore = document.getElementById('load-more');
+
+// api key
+const apiKey = 'AIzaSyDl7w6w2yH8s8Fw9XZTn8j4XG6fJ-6zZxw';
+
+let keyWord = '';
+let page = 1;
+
+async function searchImages() {
+    keyWord = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyWord}&client_id=${apiKey}`;
+}
